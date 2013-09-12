5157ddfc3520e6aaba8b8d39a96dc90f630dc663
Renee Baecker <github@renee-baecker.de>
2013-09-12 16:41:56 -0700

    add more datatypes to library

--- lib/WebService/Box/Types/Library.pm
+++ lib/WebService/Box/Types/Library.pm
@@ -5,11 +5,19 @@ use warnings;
 
 use Type::Library
     -base,
-    -declare => qw(BoxPerson PersonHash);
+    -declare => qw(
+        BoxPerson PersonHash Timestamp BoxFolderHash BoxFileHash
+        OptionalStr OptionalInt
+    );
 
 use Type::Utils -all;
 use Types::Standard -types;
 
+# create some basic types
+{
+    declare OptionalStr => as union[ Undef, Str ];
+    declare OptionalInt => as union[ Undef, Int ];
+}
 
 # create types regarding users
 {
@@ -33,17 +41,41 @@ use Types::Standard -types;
     class_type Timestamp => { class => "DateTime" };
 
     coerce Timestamp =>
-        from Str => via {
-            my ($day,$month,$year,$hour,$minute,$second) = split /[:T-]/, $_;
+        from Str() => via {
+            my ($timezone) = $_ =~ m{([+-][0-9]{2}:?[0-9]{2})\z};
+            $timezone =~ s{:}{};
+
+            my ($year,$month,$day,$hour,$minute,$second) = split /[:T-]/, $_;
             DateTime->new(
-                second => $second,
-                minute => $minute,
-                hour   => $hour,
-                day    => $day,
-                month  => $month,
-                year   => $year,
+                second    => $second,
+                minute    => $minute,
+                hour      => $hour,
+                day       => $day,
+                month     => $month,
+                year      => $year,
+                time_zone => $timezone,
             );
         };
+}
+
+{
+    declare BoxFolderHash => 
+        as Dict[
+            type        => sub{ $_ eq 'folder' },
+            id          => Str,
+            sequence_id => OptionalStr,
+            etag        => OptionalStr,
+            name        => Str,
+        ];
+
+    declare BoxFileHash => 
+        as Dict[
+            type        => sub{ $_ eq 'file' },
+            id          => Str,
+            sequence_id => OptionalStr,
+            etag        => OptionalStr,
+            name        => Str,
+        ];
 }
 
 1;
