6cd0c1c2de56043016d0a2a6ef4310bd2e98d86e
Renee Baecker <github@renee-baecker.de>
2013-09-12 16:40:59 -0700

    update attributes for files, add parent and upload method

--- lib/WebService/Box/File.pm
+++ lib/WebService/Box/File.pm
@@ -4,12 +4,20 @@ use strict;
 use warnings;
 
 use Moo;
-use Types::Standard qw(Str Int);
-use WebService::Box::Types::Library qw(BoxPerson);
+use Types::Standard qw(Str Int InstanceOf);
+use WebService::Box::Types::Library qw(BoxPerson Timestamp BoxFolderHash OptionalStr);
+use WebService::Box::Request;
 
-has [qw/type id sequence_id etag sha1 name description item_status version_number structure/] => (
+has session => (is => 'ro', isa => InstanceOf["WebService::Box::Session"], required => 1);
+
+has [qw/type id name description structure/] => (
     is  => 'ro',
     isa => Str,
+);
+
+has [qw/etag sequence_id sha1 item_status version_number/] => (
+    is  => 'ro',
+    isa => OptionalStr,
 );
 
 has [qw/size comment_count/] => (
@@ -28,6 +36,77 @@ has [qw/created_at modified_at trashed_at purged_at content_created_at content_m
     isa    => Timestamp,
     coerce => Timestamp()->coercion,
 );
+
+has parent_data => (is => 'ro', isa => BoxFolderHash);
+
+has request => (
+    is      => 'ro',
+    isa     => InstanceOf["WebService::Box::Request"], 
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        WebService::Box::Request->new( session => $self->session )
+    },
+);
+
+has _parent_object => (is => 'rwp', isa => InstanceOf["WebService::Box::Folder"]);
+
+sub parent {
+    my ($self) = @_;
+
+    my $data = $self->parent_data;
+
+    if ( !$data ) {
+        if ( !$self->id ) {
+            $self->session->box->error( 'no id for parent found and no file id exists' );
+            return;
+        }
+
+        my %file_data = $self->request(
+            ressource => 'files',
+            id        => $self->id,
+        );
+
+        $_[0] = WebService::Box::File->new( %file_data, session => $self->session );
+    }
+
+    if ( !$self->_parent_object ) {
+        my %parent_data_result = $self->request(
+            ressource => 'folders',
+            id        => $data->{id},
+        ); 
+        
+        $self->__set_parent_object(
+            WebService::Box::Folder->new( %{$data}, session => $self->session )
+        );
+    }
+
+    return $self->_parent_object;
+}
+
+sub upload {
+    my ($self, $path, $parent) = @_;
+
+    my $parent_id = ref $parent ? $parent->id : $parent;
+
+    my %upload_data = $self->request(
+        ressource => 'upload',
+        filename  => $path,
+        parent_id => $parent_id, 
+    );
+
+    my $uploaded_file = WebService::Box::File->new( %upload_data, session => $self->session );
+    return $uploaded_file;
+}
+
+sub download {
+}
+
+sub comment {
+}
+
+sub comments {
+}
 
 sub BUILDARGS {
    my ( $class, @args ) = @_;
