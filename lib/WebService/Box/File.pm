715c96b51f38788817f2a47d81f901077850c057
Renee Baecker <github@renee-baecker.de>
2013-09-13 05:23:05 -0700

    rebuild file object when attribute is accessed that hasn't been set yet: use 'around' method modifier and 'predicate' attribute setting to identify attributes that hasn't been set.

--- lib/WebService/Box/File.pm
+++ lib/WebService/Box/File.pm
@@ -4,37 +4,52 @@ use strict;
 use warnings;
 
 use Moo;
+use Sub::Identify qw(sub_name);
 use Types::Standard qw(Str Int InstanceOf);
+
 use WebService::Box::Types::Library qw(BoxPerson Timestamp BoxFolderHash OptionalStr);
 use WebService::Box::Request;
+use WebService::Box::Folder;
 
 has session => (is => 'ro', isa => InstanceOf["WebService::Box::Session"], required => 1);
 
 has [qw/type id name description structure/] => (
-    is  => 'ro',
-    isa => Str,
+    is        => 'ro',
+    isa       => Str,
+    predicate => 1,
 );
 
 has [qw/etag sequence_id sha1 item_status version_number/] => (
-    is  => 'ro',
-    isa => OptionalStr,
+    is        => 'ro',
+    isa       => OptionalStr,
+    predicate => 1,
 );
 
 has [qw/size comment_count/] => (
-    is  => 'ro',
-    isa => Int,
+    is        => 'ro',
+    isa       => Int,
+    predicate => 1,
 );
 
 has [qw/created_by modified_by owned_by/] => (
-    is     => 'ro',
-    isa    => BoxPerson,
-    coerce => BoxPerson()->coercion,
+    is        => 'ro',
+    isa       => BoxPerson,
+    coerce    => BoxPerson()->coercion,
+    predicate => 1,
 );
 
 has [qw/created_at modified_at trashed_at purged_at content_created_at content_modified_at/] => (
-    is     => 'ro',
-    isa    => Timestamp,
-    coerce => Timestamp()->coercion,
+    is        => 'ro',
+    isa       => Timestamp,
+    coerce    => Timestamp()->coercion,
+    predicate => 1,
+);
+
+has shared_link => (
+    is        => 'ro',
+    isa       => SharedLink,
+    coerce    => SharedLink()->coercion,
+    predicate => 1,
 );
 
 has parent_data => (is => 'ro', isa => BoxFolderHash);
@@ -51,6 +66,51 @@ has request => (
 
 has _parent_object => (is => 'rwp', isa => InstanceOf["WebService::Box::Folder"]);
 
+around [qw(
+    size comment_count created_by modified_by owned_by created_at modified_at trashed_at purged_at
+    content_created_at content_modified_at type name description structure shared_link
+    etag sequence_id sha1 item_status version_number
+)] => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    if ( !$self->id ) {
+        $self->session->box->error( 'invalid method call: file id does not exist, create a new object with id' );
+        return;
+    }
+
+    my $method_name = sub_name $orig;
+    my $predicate   = $self->can( 'has_' . $method_name );
+    if ( !$self->$predicate() ) {
+        $self = $_[1] = $self->rebuild;
+    }
+
+    return $self->$orig();
+};
+
+sub rebuild {
+    my ($self) = @_;
+
+    if ( !$self->id ) {
+        $self->session->box->error( 'cannot rebuild: file id does not exist' );
+        return;
+    }
+
+    my $parent_object = $self->_parent_object;
+    my %file_data = $self->request->do(
+        ressource => 'files',
+        id        => $self->id,
+    );
+
+    $_[0] = WebService::Box::File->new( %file_data, session => $self->session );
+
+    if ( $parent_object && $parent_object->id == $_[0]->parent_data->{id} ) {
+        $_[0]->_set__parent_object( $parent_object );
+    }
+
+    return $_[0];
+}
+
 sub parent {
     my ($self) = @_;
 
@@ -62,21 +122,18 @@ sub parent {
             return;
         }
 
-        my %file_data = $self->request(
+        my %file_data = $self->request->do(
             ressource => 'files',
             id        => $self->id,
         );
 
-        $_[0] = WebService::Box::File->new( %file_data, session => $self->session );
+        $self = $_[0] = WebService::Box::File->new( %file_data, session => $self->session );
     }
 
     if ( !$self->_parent_object ) {
-        my %parent_data_result = $self->request(
-            ressource => 'folders',
-            id        => $data->{id},
-        ); 
+        my $data = $self->parent_data;
         
-        $self->__set_parent_object(
+        $self->_set__parent_object(
             WebService::Box::Folder->new( %{$data}, session => $self->session )
         );
     }
@@ -89,7 +146,7 @@ sub upload {
 
     my $parent_id = ref $parent ? $parent->id : $parent;
 
-    my %upload_data = $self->request(
+    my %upload_data = $self->request->do(
         ressource => 'upload',
         filename  => $path,
         parent_id => $parent_id, 
